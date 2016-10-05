5a56faf34ec13b3db01a8a3f64a8aef2575b66c7
kelvinkipsang <kelvinkipsang@gmail.com>
2016-10-05 12:00:31 +0300

    adding .gitignore and initializing face cascade

--- run.py
+++ run.py
@@ -4,3 +4,6 @@ import sys
 # Get user supplied values
 imagePath = sys.argv[1]     #pass in the image and cascade names as command-line arguments
 cascPath = sys.argv[2]
+
+# Create the haar cascade & and initialize it with our face cascade.
+faceCascade = cv2.CascadeClassifier(cascPath)
