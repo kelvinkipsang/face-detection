ced335c72015fb664fc660012f6f508bce128961
kelvinkipsang <kelvinkipsang@gmail.com>
2016-10-05 12:03:22 +0300

    reading then converting image to grayscale

--- run.py
+++ run.py
@@ -6,4 +6,10 @@ imagePath = sys.argv[1]     #pass in the image and cascade names as command-line
 cascPath = sys.argv[2]
 
 # Create the haar cascade & and initialize it with our face cascade.
+# loads the face cascade into memory so it’s ready
 faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  #convert it to grayscale for cv2
+
